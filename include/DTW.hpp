79f6a4a55060a98cfe997235741dae7c94c80b0f
Charles Jekel <cjekel@gmail.com>
2019-08-17 14:20:38 -0400

    add documentation to DTW.hpp

--- include/DTW.hpp
+++ include/DTW.hpp
@@ -33,6 +33,18 @@ const std::string DTW_VERSION = "0.0.1";
 
 namespace DTW
 {
+  /**
+   * Compute the p_norm between two 1D c++ vectors.
+   *
+   * The p_norm is sometimes referred to as the Minkowski norm. Common 
+   * p_norms include p=2.0 for the euclidean norm, or p=1.0 for the
+   * manhattan distance. See also
+   * https://en.wikipedia.org/wiki/Norm_(mathematics)#p-norm
+   *
+   * @a 1D vector of m size, where m is the number of dimensions.
+   * @b 1D vector of m size (must be the same size as b).
+   * @p value of norm to use.
+   */
   double p_norm (std::vector<double> a, std::vector<double> b, double p) {
       double d = 0;
       for (int i = 0; i < a.size() ; i++) {
@@ -41,6 +53,21 @@ namespace DTW
       return std::pow(d, 1.0/p);
   };
 
+  /**
+   * Compute the DTW distance between two 2D c++ vectors.
+   *
+   * The c++ vectors can have different number of data points, but must
+   * have the same number of dimensions. This will raise
+   * std::invalid_argument if the dimmensions of a and b are different.
+   * Here the vectors should be formatted as
+   * [number_of_data_points][number_of_dimensions]. The DTW distance can
+   * be computed for any p_norm. See the wiki for more DTW info.
+   * https://en.wikipedia.org/wiki/Dynamic_time_warping
+   *
+   * @a 2D vector of [number_of_data_points][number_of_dimensions].
+   * @b 2D vector of [number_of_data_points][number_of_dimensions].
+   * @p value of p_norm to use.
+   */
   double dtw_distance_only(std::vector<std::vector<double>> a,
                           std::vector<std::vector<double>> b,
                           double p)
@@ -72,10 +99,22 @@ namespace DTW
     return d[n-1][o-1];
   };
 
-
+  /**
+   * Assembles a 2D c++ DTW distance vector.
+   *
+   * The DTW distance vector represents the matrix of DTW distances for
+   * all possible alignments. The c++ vectors must have the same 2D size.
+   * d.size() == c.size() == number of a data points, where d[0].size ==
+   * c[0].size() == number of b data points.
+   *
+   * @d 2D DTW distance vector of [a data points][b data points].
+   * @c 2D pairwise distance vector between every a and b data point.
+   */
   std::vector<std::vector<double> > dtw_vector_assemble(std::vector<std::vector<double>> d,
-                                                        std::vector<std::vector<double>> c,
-                                                        int n, int o) {
+                                                        std::vector<std::vector<double>> c) 
+  {
+    int n = d.size();
+    int o = d[0].size();
     for (int i=1; i < n; i++)
       {
         d[i][0] = d[i-1][0] + c[i][0];
@@ -97,14 +136,36 @@ namespace DTW
   class DTW {
 
   public:
-    /* Compute the Dynamic Time Warping distance between two curves in space
-
-    */
-
     std::vector<std::vector<double> > a_vector, b_vector;
     int a_data, n_dim, b_data;
     double p, distance;
     std::vector<std::vector<double> > dtw_vector, pairwise_vector;
+
+    /**
+      * Class for Dynamic Time Warping distance between two 2D c++ vectors.
+      * 
+      * The c++ vectors can have different number of data points, but must
+      * have the same number of dimensions. This will raise
+      * std::invalid_argument if the dimmensions of a and b are different.
+      * Here the vectors should be formatted as
+      * [number_of_data_points][number_of_dimensions]. The DTW distance can
+      * be computed for any p_norm. See the wiki for more DTW info.
+      * https://en.wikipedia.org/wiki/Dynamic_time_warping
+      * 
+      * @a 2D vector of [number_of_data_points][number_of_dimensions].
+      * @b 2D vector of [number_of_data_points][number_of_dimensions].
+      * @p value of p_norm to use.
+      * 
+      * This class stores the following:
+      * 
+      * @DTW.distance Computed DTW distance.
+      * @DTW.pairwise_vector P_norm distance between each a and b data point.
+      * @DTW.dtw_vector DTW distance matrix.
+      * 
+      * The class has the following methods:
+      * 
+      * @DTW.path() Returns a 2D vector of the alignment path between a and b.
+      */
     DTW (std::vector<std::vector<double> > a, std::vector<std::vector<double> > b, double p) : 
       a_vector(a), b_vector(b), p(p) {
         a_data = a.size();
@@ -131,9 +192,18 @@ namespace DTW
         pairwise_vector = c;
         std::vector<std::vector<double> > d(a_data, std::vector<double> (b_data, 0.0));
         d[0][0] = pairwise_vector[0][0];
-        dtw_vector = dtw_vector_assemble(d, pairwise_vector, a_data, b_data);
+        dtw_vector = dtw_vector_assemble(d, pairwise_vector);
         distance = dtw_vector[a_data-1][b_data-1];
       };
+
+    /**
+     * Returns a 2D vector of the alignment path between a and b.
+     *
+     * The DTW path is a 2D integer vector, where [path_length][i] represents
+     * the i'th data point on curve a, and [path_length][j] represents the j'th
+     * data point on curve b. The path_length will depend upon the optimal DTW
+     * alignment.
+     */
     std::vector<std::vector<int> > path () {
       int i = a_data - 1;
       int j = b_data - 1;
